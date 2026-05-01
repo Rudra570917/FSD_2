40fa06eab9127a570ecd431fd03bd59ef408217e
Rudra570917 <rudrabhavsar26@gmail.com>
2026-05-01 08:47:14 +0530

    Files added

---
+++ Task/src/server.js
@@ -0,0 +1,9 @@
+var express=require("express")
+var app=express()
+app.set("view engine","ejs")
+app.get('/',(req,res)=>{
+    res.render('first')
+})
+app.listen(5663,()=>{
+    console.log("Server")
+})
